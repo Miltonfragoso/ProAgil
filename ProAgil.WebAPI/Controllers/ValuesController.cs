dbece07b045241fb036f651311d250bd88ceaa7b
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2023-11-08 00:43:07 -0300

    Implementando método Get retornar dados do BD

--- ProAgil.WebAPI/Controllers/ValuesController.cs
+++ ProAgil.WebAPI/Controllers/ValuesController.cs
@@ -4,38 +4,28 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ProAgil.WebAPI.Data;
 using ProAgil.WebAPI.Model;
 
 namespace ProAgil.WebAPI.Controllers
 {
+
+
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        public readonly DataContext context;
+        public ValuesController(DataContext context)
+        {
+                this.context = context;
+            
+        }
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<Evento>> Get()
         {
-            return new Evento[] {
-
-                new Evento(){
-                    EventoId = 1,
-                    Tema = "Angular e .NET Core",
-                    Local = "Belo Horizonte",
-                    Lote = "1° Lote",
-                    QtdPessoas = 250,
-                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
-                },
-
-                new Evento(){
-                    EventoId = 2,
-                    Tema = "Angular e suas novidades",
-                    Local = "São Paulo",
-                    Lote = "2° Lote",
-                    QtdPessoas = 350,
-                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy")
-                },
-             };
+            return context.Eventos.ToList();
         }
 
         // GET api/values/5
