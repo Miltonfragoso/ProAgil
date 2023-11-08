c1f01ec6cc2c28e5538af3edf3afd97e78f7190b
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2023-11-08 00:01:27 -0300

    First Commit

---
+++ ProAgil.WebAPI/Data/DataContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using ProAgil.WebAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ProAgil.WebAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base  (options) {}
+
+         public DbSet<Evento> Eventos { get; set; } 
+
+    }
+}
